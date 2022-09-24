5919e019c739bc8b611bc4997dffe4045c23a0b8
Magdalena <mkceva@gmail.com>
2022-09-24 23:26:09 +0200

    author and book- equals and tostring

--- src/main/java/com/example/springbootwebapp/domain/Book.java
+++ src/main/java/com/example/springbootwebapp/domain/Book.java
@@ -1,6 +1,7 @@
 package com.example.springbootwebapp.domain;
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -55,5 +56,28 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
