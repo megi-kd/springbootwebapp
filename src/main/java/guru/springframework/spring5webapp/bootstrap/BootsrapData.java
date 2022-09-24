dca118fbe74bcbea0a6fb0ae077a762fddddec03
Magdalena <mkceva@gmail.com>
2022-09-25 00:20:54 +0200

    adding publisher

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootsrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootsrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repository.AuthorRepository;
 import guru.springframework.spring5webapp.repository.BookRepository;
+import guru.springframework.spring5webapp.repository.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,10 +13,13 @@ import org.springframework.stereotype.Component;
 public class BootsrapData implements CommandLineRunner {
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootsrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootsrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+                        PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -35,8 +40,12 @@ public class BootsrapData implements CommandLineRunner {
         authorRepository.save(sss);
         bookRepository.save(book2);
 
+        Publisher publisher = new Publisher("Publusher1", "Address", "city", "state", "zipcode");
+        publisherRepository.save(publisher);
+
         System.out.println("Started in Bootstrap");
         System.out.println("Number of books:" + bookRepository.count());
+        System.out.println(("Number of publishers: "+ publisherRepository.count()));
 
 
     }
