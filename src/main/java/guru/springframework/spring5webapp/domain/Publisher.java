ee33e45a256b1b9841fec8bb944a20dcecc881ce
Magdalena <mkceva@gmail.com>
2022-09-25 23:00:46 +0200

    tostring fix

--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -96,7 +96,6 @@ public class Publisher {
                 ", city='" + city + '\'' +
                 ", state='" + state + '\'' +
                 ", zip='" + zip + '\'' +
-                ", books=" + books +
                 '}';
     }
 
